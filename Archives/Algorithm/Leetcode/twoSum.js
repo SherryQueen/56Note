4de774fa19ba4ffff2c43d6088ee74a91a7c88d7
MySherry <sherryqueenlove@gmail.com>
2020-12-10 18:48:53 +0800

    Create twoSum.js

---
+++ Archives/Algorithm/Leetcode/twoSum.js
@@ -0,0 +1,20 @@
+/**
+ * @filename    twoSum.js
+ * @author      56
+ * @description https://leetcode-cn.com/problems/two-sum/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  const map = {};
+  for (let i = 0, len = nums.length; i < len; i++) {
+    const n = nums[i];
+    if (map[n] !== undefined) return [map[n], i];
+    map[target - n] = i;
+  }
+  return [];
+};
