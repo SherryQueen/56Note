3f439c6e6dba08bd687bfcef4b3ecdc5a05386aa
56 <sherryqueenlove@gmail.com>
2020-12-16 15:21:30 +0800

    feat: Chrome tools

---
+++ Archives/Toys/chrome_tools/src/zhihu_reader.js
@@ -0,0 +1,16 @@
+(function () {
+  document.querySelectorAll('.Post-NormalMain header').forEach((node) => (node.style.width = '80%'));
+  document.querySelectorAll('.Post-NormalMain>div').forEach((node) => (node.style.width = '80%'));
+
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  style.rel = 'stylesheet';
+  style.appendChild(
+    document.createTextNode(`
+    .Post-SideActions {
+      left: -9999999px !important;
+    }
+  `)
+  );
+  document.getElementsByTagName('head')[0].appendChild(style);
+})();
