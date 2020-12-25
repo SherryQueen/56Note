412202440aa948ac34b17218656018fb49060afc
MySherry <sherryqueenlove@gmail.com>
2020-12-25 11:15:05 +0800

    Update imageToBase64.js

--- Archives/Toys/Image/imageToBase64.js
+++ Archives/Toys/Image/imageToBase64.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
+const path = require('path')
 
-const image = fs.readFileSync('./bg.jpeg')
+const image = fs.readFileSync(path.join(__dirname, './bg.jpeg'))
 const base64 = Buffer.from(image, 'binary').toString('base64')
-fs.writeFileSync('result.txt', 'data:image/jpeg;base64,' + base64)
+fs.writeFileSync(path.join(__dirname, 'result.txt'), 'data:image/jpeg;base64,' + base64)
