92d491bf2aabf0a0e5ff8e4b500d1c2b503957e9
MySherry <mysherrymyqueen@outlook.com>
2023-10-30 21:21:45 -0500

    Update requestIdempotent.ts

--- Toys/requestIdempotent.ts
+++ Toys/requestIdempotent.ts
@@ -1,4 +1,5 @@
-const sleep = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout))
+const sleep = (timeout) =>
+  new Promise((resolve) => setTimeout(resolve, timeout));
 
 interface IOption {
   timeout?: number;
@@ -7,73 +8,76 @@ interface IOption {
 
 interface IMemoryItem {
   loading: boolean;
-  promise: Promise<any>;
+  promise: Promise<unknown>;
   timeout: number;
   startTime: number;
 }
 
-export const requestIdempotent = <R = any>(fn: (...args: any[]) => Promise<any>, opt: IOption = {}) => {
+export const requestIdempotent = <R = unknown>(
+  fn: (...args: unknown[]) => Promise<R>,
+  opt: IOption = {}
+) => {
   const memory = new Map<string, IMemoryItem>();
-  const timeout = typeof opt.timeout === 'undefined' ? -1 : opt.timeout;
-  const ignoreQuery = typeof opt.ignoreQuery === 'undefined' ? false : opt.ignoreQuery;
+  const timeout = typeof opt.timeout === "undefined" ? -1 : opt.timeout;
+  const ignoreQuery =
+    typeof opt.ignoreQuery === "undefined" ? false : opt.ignoreQuery;
 
-  return async (...args: any[]): Promise<R> => {
-    const key = ignoreQuery ? 'ignoreQuery' : JSON.stringify(args);
+  return async (...args: unknown[]): Promise<R> => {
+    const key = ignoreQuery ? "ignoreQuery" : JSON.stringify(args);
 
     const ans = memory.get(key);
-    console.info('request once key', args, key, ans);
+    console.info("request once key", args, key, ans);
     if (!ans) return startFetch();
-    if (ans.loading) return ans.promise;
-    if (ans.timeout === -1 || Date.now() - ans.startTime <= timeout) return ans.promise;
+    if (ans.loading) return ans.promise as R;
+    if (ans.timeout === -1 || Date.now() - ans.startTime <= timeout)
+      return ans.promise as R;
     // * Restart request
     return startFetch();
 
-    async function startFetch() {
+    async function startFetch(): Promise<R> {
       const ans: IMemoryItem = {
         loading: true,
-        promise: fn.apply(null, args),
+        promise: fn(...args),
         timeout,
         startTime: 0,
       };
       memory.set(key, ans);
 
       try {
-        const data = await ans.promise;
+        const data = (await ans.promise) as R;
         ans.loading = false;
         ans.startTime = Date.now();
         return data;
       } catch (err) {
         memory.delete(key);
         throw err;
-      } finally {
       }
     }
   };
 };
 
-
 const fun1 = async (query) => {
-  await sleep(1000)
-  console.log('exec fun1 once')
-  return `result of fun1(${query})`
-}
+  await sleep(1000);
+  console.log("exec fun1 once");
+  return `result of fun1(${query})`;
+};
 
 const fun2 = async (query) => {
-  await sleep(1000)
-  console.log('exec fun2 once')
-  return `result of fun2(${query})`
-}
+  await sleep(1000);
+  console.log("exec fun2 once");
+  return `result of fun2(${query})`;
+};
 
-const fn1 = requestIdempotent(fun1)
-const fn2 = requestIdempotent(fun2)
+const fn1 = requestIdempotent(fun1);
+const fn2 = requestIdempotent(fun2);
 
 const test = async () => {
-  let data
-  data = await Promise.all([fn1('1'), fn1('1'), fn1('2'), fn2('1')])
-  console.log('data', data)
-  await sleep(2000)
-  data = await fn1('1')
-  console.log('data', data)
-}
+  let data;
+  data = await Promise.all([fn1("1"), fn1("1"), fn1("2"), fn2("1")]);
+  console.log("data", data);
+  await sleep(2000);
+  data = await fn1("1");
+  console.log("data", data);
+};
 
-test()
+test();
